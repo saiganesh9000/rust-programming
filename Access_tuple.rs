9efc4067df5e006fab8ce338d5bb7673b836db08
saiganesh9000 <155514093+saiganesh9000@users.noreply.github.com>
2025-07-11 09:57:57 +0530

    Add files via upload

---
+++ Access_tuple.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let tup = ("Rust", 2024, 3.14);
+
+    println!("Tuple: {:?}", tup);
+    println!("First element: {}", tup.0);
+    println!("Second element: {}", tup.1);
+    println!("Third element: {}", tup.2);
+}
