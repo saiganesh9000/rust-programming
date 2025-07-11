a684d0834936fe634fe5333f4f51289b0db7d497
saiganesh9000 <155514093+saiganesh9000@users.noreply.github.com>
2025-07-11 10:10:00 +0530

    Create constants.rs

---
+++ constants.rs
@@ -0,0 +1,9 @@
+const PI: f64 = 3.1415;
+const MAX_LIMIT: i32 = 1000;
+const SITE_NAME: &str = "RustLang.org";
+
+fn main() {
+    println!("PI = {}", PI);
+    println!("Max Limit = {}", MAX_LIMIT);
+    println!("Site Name = {}", SITE_NAME);
+}
