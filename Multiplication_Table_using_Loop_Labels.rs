d105de8bdc56d1d0ee88bd401389d2baaf7d53df
saiganesh9000 <155514093+saiganesh9000@users.noreply.github.com>
2025-07-18 10:12:11 +0530

    Create Multiplication_Table_using_Loop_Labels.rs

---
+++ Multiplication_Table_using_Loop_Labels.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let number = 5;
+
+    'outer: for i in 1..=10 {
+        println!("{} x {} = {}", number, i, number * i);
+        if i == 10 {
+            break 'outer;
+        }
+    }
+}
